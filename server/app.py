2e887910903fed7243c5306acdb9e033e3b915b4
i770 <diegomudaida@gmail.com>
2025-03-27 10:05:57 +0300

    initial commit

--- server/app.py
+++ server/app.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, request, current_app, make_response
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    host = request.headers.get('Host')
+    appname = current_app.name
+    response_body = f'''
+        <h1>The host for this page is {host}</h1>
+        <h2>The name of this application is {appname}</h2>
+    '''
+
+    status_code = 200
+    headers = {}
+
+    return make_response(response_body, status_code, headers)
+
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5556, debug=True)
